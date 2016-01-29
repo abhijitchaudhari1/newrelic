336ce5537b1dc0b7f186e3dec73a8154e09c25d0
initcron-abhijit <root@ip-10-1-11-61.eu-west-1.compute.internal>
2016-01-29 10:22:55 -0500

    modifying filster script

--- get.sh
+++ get.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PATH=/usr/local/apache-maven/bin:/usr/local/tomcat:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 unset del
-del=$(gluster volume profile ${1} info | grep -n "Brick"  | sed -n 2p | cut -d":" -f 1) 
+del=`gluster volume profile ${1} info | grep -n "Brick"  | sed -n 2p | cut -d":" -f 1`
+echo -e "\e[32mDel value is $del\e[m"
 gluster volume profile ${1} info | sed -n "1,${del}p"
